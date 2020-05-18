6837d65c5cb6296b6012047525595cbcac1caa98
yotemuku <63775103+yotemuku@users.noreply.github.com>
2020-05-18 16:04:47 +0900

    update main.py aiko

--- tool/main.py
+++ tool/main.py
@@ -94,8 +94,8 @@ class DataJson:
             data["リリース日"] = release_date.isoformat() + ".000Z"
             data["曜日"] = self.patients_sheet.cell(row=i, column=2).value
             data["居住地"] = self.patients_sheet.cell(row=i, column=5).value
-            if self.patients_sheet.cell(row=i, column=5).value != "dummy":
-                data["居住地"] = data["居住地"]
+            if self.patients_sheet.cell(row=i, column=5).value != "大阪府外":
+                data["居住地"] = "大阪府" + data["居住地"]
             data["年代"] = str(self.patients_sheet.cell(row=i, column=3).value) + (
                 "代" if isinstance(self.patients_sheet.cell(row=i, column=3).value, int) else ""
             )
